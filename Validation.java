59a6f207f27ac7c1a66a84eb82735b1f83cba814
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-08-04 10:33:00 +0530

    Update Validation.java

--- Validation.java
+++ Validation.java
@@ -3,22 +3,18 @@ import java.util.*;
 class Main {
 
     public static String UsernameValidation(String str) {
-       
         if (str.length() < 4 || str.length() > 25) {
             return "false";
         }
 
-        
         if (!Character.isLetter(str.charAt(0))) {
             return "false";
         }
 
-       
         if (!str.matches("^[a-zA-Z0-9_]+$")) {
             return "false";
         }
 
-       
         if (str.charAt(str.length() - 1) == '_') {
             return "false";
         }
@@ -28,7 +24,12 @@ class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print(UsernameValidation(s.nextLine()));
-    }
+        int numTestCases = s.nextInt();
+        s.nextLine();
 
+        for (int i = 0; i < numTestCases; i++) {
+            String username = s.nextLine();
+            System.out.println(UsernameValidation(username));
+        }
+    }
 }
