562851be71cb3b916db94c1619f713b0ab00edfa
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-08-04 10:18:08 +0530

    Create Validation.java

---
+++ Validation.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Main {
+
+    public static String CodelandUsernameValidation(String str) {
+        // Check if the username length is between 4 and 25 characters.
+        if (str.length() < 4 || str.length() > 25) {
+            return "false";
+        }
+
+        // Check if the username starts with a letter.
+        if (!Character.isLetter(str.charAt(0))) {
+            return "false";
+        }
+
+        // Check if the username contains only letters, numbers, and underscores.
+        if (!str.matches("^[a-zA-Z0-9_]+$")) {
+            return "false";
+        }
+
+        // Check if the username ends with an underscore character.
+        if (str.charAt(str.length() - 1) == '_') {
+            return "false";
+        }
+
+        // If all checks pass, return "true".
+        return "true";
+    }
+
+    public static void main(String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(CodelandUsernameValidation(s.nextLine()));
+    }
+
+}
